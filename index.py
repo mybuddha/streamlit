bc1428f65187b5beaec928732d4bf05b781dba3d
mybuddha <mybuddha@users.noreply.github.com>
2024-01-17 13:28:30 +0400

    Update index.py

--- index.py
+++ index.py
@@ -3,4 +3,17 @@ import streamlit as st
 st.write("""
 # Uber pickups in **NYC** *app*!'
 """)
+# 1. as Sidebar menu
+with st.sidebar:
+  selected = option_menu(
+    menu_title="Main Menu", #required
+    option=["Home", "Projects", "Contact"]
+  )
+  st.title(f"You have selected {selected}")
+if selected == "Home":
+  st.title(f"You have selected {selected}")
+if selected == "Projects":
+  st.title(f"You have selected {selected}")
+if selected == "Contact":
+  st.title(f"You have selected {selected}")
 
