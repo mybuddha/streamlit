35e8edfed211c3540832a1a2098212504cedc5f8
mybuddha <mybuddha@users.noreply.github.com>
2024-01-17 13:08:20 +0400

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
 
+# 1. as Sidebar menu
+with st.sidebar:
+  selected = option_menu(
+    menu_title="Main Menu", #required
+    option=["Home", "Projects", "Contact"]
+  )
+
+if selected == "Home":
+  st.title(f"You have selected {selected}")
+if selected == "Projects":
+  st.title(f"You have selected {selected}")
+if selected == "Contact":
+  st.title(f"You have selected {selected}")
